c5f3ed980be467a0209a9660ef178d17eb3982e5
ZineddineBK-DEV <55902255+ZineddineBK-DEV@users.noreply.github.com>
2021-07-16 15:30:39 +0200

    Update work.php

--- php/work.php
+++ php/work.php
@@ -25,38 +25,7 @@
    <div style="padding: 100px;background-color: #d9e0e0;">
 	<article class='gallery'>
 		<?php
-		//displays only .jpg images
-		$dir_path = "assets/images/";
-		$extensions_array = array('jpg');
-		
-		if(is_dir($dir_path))
-		{
-			$files = scandir($dir_path);
-			
-			for($i = 0; $i < count($files); $i++)
-			{
-				if($files[$i] !='.' && $files[$i] !='..')
-				{
-					// get file name
-					//echo "File Name -> $files[$i]<br>";
-					
-					// get file extension
-					$file = pathinfo($files[$i]);
-					$extension = $file['extension'];
-					//echo "File Extension-> $extension<br>";
-					
-				   // check file extension
-					if(in_array($extension, $extensions_array))
-					{
-					// show image
-					echo "  <figure class='gallery-image'>
-							<img src='$dir_path$files[$i]' style='width:1000;height:1000;'>
-							</figure>";
-					}
-				}
-			}
-		
-		}
+		include('fileList.php');
 		?>	
 	</article>
 	</div>  
